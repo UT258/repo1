8b44c4ac886688fb42b631f4c08c4ee4db9940a7
UT258 <utkarshsingh404499@gmail.com>
2023-09-14 15:48:49 +0530

    changed Node to TreeNode

---
+++ binaryTree.java
@@ -0,0 +1,21 @@
+class binaryTree
+{
+     class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+     
+        //constructor
+        public TreeNode (int val)
+        {
+            this.val=val;
+        }
+
+    }
+    TreeNode root;
+    
+    public void insert(int val)
+    {
+
+    }
+}
